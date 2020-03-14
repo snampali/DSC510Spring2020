fb14750d4652e3741248ff8e710348f89244f22b
anthony.larosa@knlsolutions.com <anthony.larosa@knlsolutions.com>
2020-03-14 14:16:23 -0400

    Updated hello_world comments

---
+++ Anthony_LaRosa_DSC510/larosa_hello_world.py
@@ -0,0 +1,8 @@
+#Anthony LaRosa
+#03142020
+#DSC510-Week1
+#Version 1
+
+#Have Python Print hello world!
+#I originally used single quotes and it works, but the IDE gave me feedback to use double quotes so I switched it to double.
+print("hello world")
