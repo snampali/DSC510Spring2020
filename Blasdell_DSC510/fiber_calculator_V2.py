730f093c04c5b5c4548e68060adbd8442e573884
Blazer514 <61950301+Blazer514@users.noreply.github.com>
2020-03-21 13:24:32 -0400

    Update Hello World

--- Blasdell_DSC510/fiber_calculator_V2.py
+++ Blasdell_DSC510/fiber_calculator_V2.py
@@ -15,9 +15,8 @@ company_name = input('Please enter your company name: ')
 print('\r')
 
 # input customer's cable need
-cable_feet = float(input('Please enter the amount of fiber you need (in feet): '))
+cable_feet: float = float(input('Please enter the amount of fiber you need (in feet): '))
 print('\r')
-
 
 # set default cable price
 default_price = 0.87
@@ -27,15 +26,16 @@ cable_price = default_price
 # > 100 feet = 0.80
 # > 250 feet = 0.70
 # > 500 feel = 0.50
-if cable_feet > 500:
+
+if cable_feet > 500.0:
     cable_price = 0.50
-elif cable_feet > 250:
+elif cable_feet > 250.0:
     cable_price = 0.70
-elif cable_price > 100:
+elif cable_feet > 100.0:
     cable_price = 0.80
 
 # calculate customer price feet * default price
-installation_cost = cable_feet * cable_price
+installation_cost = format(cable_feet * cable_price, '.2f')
 
 # receipt display
 print('\r')
@@ -46,8 +46,8 @@ print('Receipt\r')
 print('\r')
 print('\r')
 print('Company: ', company_name)
-print('Fiber amount (in feet)', cable_feet)
-print('Cable price: $ ', cable_price)
+print('Fiber optic cable amount (in feet)', cable_feet)
+print('Fiber Cable installation price: $ ', format(cable_price, '.2f'))
 print('\r')
 print('Installation total: $', installation_cost)
 print('\r')
